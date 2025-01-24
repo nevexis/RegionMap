f44d32aa9722a5da769ef44baeb79828dfa075e9
Nevah5 <contact@nevah5.dev>
2025-01-24 10:37:34 +0100

    Initial

---
+++ src/client/java/dev/nevah5/nevexis/regionmap/RegionMapClient.java
@@ -0,0 +1,11 @@
+package dev.nevah5.nevexis.regionmap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RegionMapClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+	}
+}
