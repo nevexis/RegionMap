987a11d5b477dcaf103beb7809f2be9ca7f7c5b4
Nevah5 <contact@nevah5.dev>
2025-01-26 21:50:53 +0100

    Store regions persistently

--- src/main/java/dev/nevah5/nevexis/regionmap/RegionMap.java
+++ src/main/java/dev/nevah5/nevexis/regionmap/RegionMap.java
@@ -1,6 +1,7 @@
 package dev.nevah5.nevexis.regionmap;
 
 import de.bluecolored.bluemap.api.BlueMapAPI;
+import dev.nevah5.nevexis.regionmap.api.BlueMapApiImpl;
 import dev.nevah5.nevexis.regionmap.command.RegionCommand;
 import net.fabricmc.api.ModInitializer;
 
@@ -8,22 +9,30 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class RegionMap implements ModInitializer {
 	public static final String MOD_ID = "regionmap";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String REGION_MAP_CONFIG_DIRECTORY = "config/regionmap/";
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("RegionMap by Nevah5 initialized!");
 
+		Path configDirectory = Paths.get(REGION_MAP_CONFIG_DIRECTORY);
+		try {
+			if (Files.notExists(configDirectory)) {
+				Files.createDirectories(configDirectory);
+				LOGGER.info("Config directory created at: " + configDirectory.toAbsolutePath());
+			}
+		} catch (IOException ex) {
+			LOGGER.error("Failed to create config directory", ex);
+		}
+		BlueMapApiImpl.setup();
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
 			RegionCommand.register(dispatcher);
--- src/main/java/dev/nevah5/nevexis/regionmap/api/BlueMapApiImpl.java
+++ src/main/java/dev/nevah5/nevexis/regionmap/api/BlueMapApiImpl.java
@@ -3,16 +3,30 @@ package dev.nevah5.nevexis.regionmap.api;
 import com.flowpowered.math.vector.Vector2d;
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import de.bluecolored.bluemap.api.BlueMapMap;
+import de.bluecolored.bluemap.api.gson.MarkerGson;
 import de.bluecolored.bluemap.api.markers.ExtrudeMarker;
 import de.bluecolored.bluemap.api.markers.MarkerSet;
 import de.bluecolored.bluemap.api.math.Color;
 import de.bluecolored.bluemap.api.math.Shape;
+import dev.nevah5.nevexis.regionmap.RegionMap;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class BlueMapApiImpl implements BlueMapApi {
+    public static final Logger LOGGER = LoggerFactory.getLogger(RegionMap.MOD_ID);
+    private static final String REGION_DIRECTORY = RegionMap.REGION_MAP_CONFIG_DIRECTORY + "regions/";
     private static final float EXTRUDE_FROM = -64;
     private static final float EXTRUDE_TO = 319;
+
     private BlueMapAPI api;
 
     public BlueMapApiImpl() {
@@ -21,8 +35,32 @@ public class BlueMapApiImpl implements BlueMapApi {
         );
     }
 
+    public static void setup() {
+        Path configDirectory = Paths.get(REGION_DIRECTORY);
+        try {
+            if (Files.notExists(configDirectory)) {
+                Files.createDirectories(configDirectory);
+                LOGGER.info("Config directory created at: " + configDirectory.toAbsolutePath());
+            }
+        } catch (IOException ex) {
+            LOGGER.error("Failed to create config directory", ex);
+        }
+    }
+
     @Override
     public void addRegion(final World world, final ChunkPos pos, final String name) {
+        final Path markerSetPath = Paths.get(REGION_DIRECTORY + name.toLowerCase() + ".json");
+        MarkerSet markerSet = MarkerSet.builder()
+                .label(name + "'s Regions")
+                .build();
+        if (Files.exists(markerSetPath)) {
+            try (FileReader reader = new FileReader(markerSetPath.toString())) {
+                markerSet = MarkerGson.INSTANCE.fromJson(reader, MarkerSet.class);
+            } catch (IOException ex) {
+                LOGGER.error("Failed to load region marker set", ex);
+            }
+        }
+
         int chunkX = pos.x;
         int chunkZ = pos.z;
 
@@ -33,7 +71,7 @@ public class BlueMapApiImpl implements BlueMapApi {
 
         // TODO: add dimension support
         final ExtrudeMarker marker = new ExtrudeMarker.Builder()
-                .label(name)
+                .label("Chunk " + chunkX + ", " + chunkZ)
                 .shape(Shape.builder()
                         .addPoint(toPoint(minX, minZ))
                         .addPoint(toPoint(maxX, minZ))
@@ -45,18 +83,21 @@ public class BlueMapApiImpl implements BlueMapApi {
                 .fillColor(new Color(255, 0, 0, 0.1f))
                 .build();
 
-        final MarkerSet markerSet = MarkerSet.builder()
-                .label(name)
-                .build();
-
         markerSet.getMarkers()
-                .put("my-marker-id", marker);
+                .put(name.toLowerCase() + "-chunk-" + chunkX + "-" + chunkZ, marker);
 
+        MarkerSet finalMarkerSet = markerSet;
         api.getWorld(world).ifPresent(bmWorld -> {
             for (BlueMapMap map : bmWorld.getMaps()) {
-                map.getMarkerSets().put("my-marker-set-id", markerSet);
+                map.getMarkerSets().put(name.toLowerCase() + "-regions", finalMarkerSet);
             }
         });
+
+        try (FileWriter writer = new FileWriter(REGION_DIRECTORY + name.toLowerCase() + ".json")) {
+            MarkerGson.INSTANCE.toJson(finalMarkerSet, writer);
+        } catch (IOException ex) {
+            LOGGER.error("Failed to save region marker set", ex);
+        }
     }
 
     private Vector2d toPoint(double x, double z) {
