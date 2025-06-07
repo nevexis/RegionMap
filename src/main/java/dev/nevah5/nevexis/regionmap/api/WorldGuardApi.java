0535f2019eb1fe2b38d0d046e5e09a5ef26659b1
Nevah5 <contact@nevah5.dev>
2025-06-07 16:49:30 +0200

    WIP

--- src/main/java/dev/nevah5/nevexis/regionmap/api/WorldGuardApi.java
+++ src/main/java/dev/nevah5/nevexis/regionmap/api/WorldGuardApi.java
@@ -1,4 +1,7 @@
 package dev.nevah5.nevexis.regionmap.api;
 
+import org.joml.Vector2d;
+
 public interface WorldGuardApi {
+    void claimChunk(Vector2d chunk);
 }
--- src/main/java/dev/nevah5/nevexis/regionmap/api/WorldGuardApiImpl.java
+++ src/main/java/dev/nevah5/nevexis/regionmap/api/WorldGuardApiImpl.java
@@ -1,4 +1,11 @@
 package dev.nevah5.nevexis.regionmap.api;
 
+import de.z0rdak.yawp.api.core.RegionManager;
+import org.joml.Vector2d;
+
 public class WorldGuardApiImpl implements WorldGuardApi {
+    @Override
+    public void claimChunk(Vector2d chunk) {
+        // TODO: here
+    }
 }
